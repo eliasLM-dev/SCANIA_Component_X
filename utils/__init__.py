c5c6140d2ead9139b8f9ef71bbb8ea6f8bccbc5d
Elias <eliaslarsson@outlook.com>
2026-04-29 01:04:33 +0200

    Fixed overfitting models, preprocessing and graphs.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from .trainer import EarlyStopper, BaseTrainer
+from .trainer import EarlyStopper, BaseTrainer, random_search, DEVICE
 from .models import LSTMModel, TCNModel, TapNetEncoder
 from .data_utils import get_cumulative_cols, prepare_lr_data, generate_sequential_data,compute_total_cost, get_metrics
